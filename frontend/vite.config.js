25ef1a71aaeab9f45920fa5743555dc0ecfbd1d4
Rowena Pacadalhin <110021697+iamrowena@users.noreply.github.com>
2025-08-13 17:37:14 +0800

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,6 +5,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/Doctor-Appointment-Booking/',
+  base: '/Doctor-Appointment-Booking/frontend/',
   plugins: [react(), tailwindcss()],
 })
